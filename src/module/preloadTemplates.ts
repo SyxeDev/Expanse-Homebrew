14b41dd276edddc4cf90f00c094cc1a2f11b17c8
SyxeDev <thesyxe@gmail.com>
2021-04-15 14:20:34 -0500

    v0.0.4

--- src/module/preloadTemplates.ts
+++ src/module/preloadTemplates.ts
@@ -1,7 +1,8 @@
-export const preloadTemplates = async function() {
-	const templatePaths = [
-		// Add paths to "systems/ExpanseHomebrew/templates"
-	];
+export const preloadTemplates = async function () {
+  const templatePaths = [
+    // Add paths to "systems/ExpanseHomebrew/templates"
+    "systems/ExpanseHomebrew/templates/sheets/crew-sheet.hbs",
+  ];
 
-	return loadTemplates(templatePaths);
-}
+  return loadTemplates(templatePaths);
+};
